b4d8f8525d66c726a5f9db7ae9289faf7b9d9eff
Zaid Ali <168811833+ZAIDDUARTE@users.noreply.github.com>
2024-09-03 15:20:36 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -184,7 +184,7 @@ export default function Home() {
               Book a Call
             </Link>
             <Link
-              href={"/showcase"}
+              href={"https://property.omniwebmarketing.com/"}
               className="
               bg-white
    py-3 
@@ -196,7 +196,7 @@ export default function Home() {
         rounded-[6px]
         hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)]"
             >
-              Showcase
+              Get Estimate
             </Link>
           </div>
 
